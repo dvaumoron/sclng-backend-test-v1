82a9941a35c4ae8f8882868dbbeaba399de0b66e
Denis Vaumoron <dvaumoron@gmail.com>
2023-12-28 09:16:11 +0100

    repository service

--- repositoryservice/repository.go
+++ repositoryservice/repository.go
@@ -13,23 +13,27 @@ import (
 )
 
 type empty = struct{}
+type JsonObject = map[string]any
 
-type Repository struct {
-	FullName  string                    `json:"full_name"`
-	Owner     string                    `json:"owner"`
-	Name      string                    `json:"repository"`
-	Languages map[string]map[string]int `json:"languages"`
-}
+type RepositoryService <-chan []JsonObject
 
-func MakeRepository(fullName string, owner string, name string, languages map[string]int) Repository {
-	converted := make(map[string]map[string]int, len(languages))
-	for name, bytes := range languages {
-		converted[name] = map[string]int{"bytes": bytes}
+var (
+	marker = empty{}
+
+	keepField = map[string]empty{
+		"name": marker, "full_name": marker, "description": marker, "forks_count": marker, "watchers_count": marker, "topics": marker,
 	}
-	return Repository{FullName: fullName, Owner: owner, Name: name, Languages: converted}
-}
 
-type RepositoryService <-chan []Repository
+	flattenField = map[string]string{
+		"owner": "login", "licenses": "key", "organization": "login",
+	}
+
+	fetchField = map[string]string{
+		"languages_url": "languages",
+	}
+
+	cleanedSize = len(keepField) + len(flattenField) + len(fetchField)
+)
 
 func Make(log logrus.FieldLogger, eventApiUrl string, eventPageSize int, refresh time.Duration, maxCall int, accessToken string) RepositoryService {
 	var urlBuilder strings.Builder
@@ -42,18 +46,18 @@ func Make(log logrus.FieldLogger, eventApiUrl string, eventPageSize int, refresh
 	authorizationBuilder.WriteString("Bearer ")
 	authorizationBuilder.WriteString(accessToken)
 
-	repositoriesChan := make(chan []Repository)
+	repositoriesChan := make(chan []JsonObject)
 	go manageUpdate(log, repositoriesChan, urlBuilder.String(), refresh, maxCall, authorizationBuilder.String())
 	return repositoriesChan
 }
 
-func (rs RepositoryService) List() []Repository {
+func (rs RepositoryService) List() []JsonObject {
 	return <-rs
 }
 
-func manageUpdate(log logrus.FieldLogger, repositoriesChan chan<- []Repository, eventPageUrl string, refresh time.Duration, maxCall int, authorizationHeader string) {
+func manageUpdate(log logrus.FieldLogger, repositoriesChan chan<- []JsonObject, eventPageUrl string, refresh time.Duration, maxCall int, authorizationHeader string) {
 	repositoriesCache := retrieveRepositoriesData(log, eventPageUrl, maxCall, authorizationHeader)
-	repositoriesUpdateChan := make(chan []Repository)
+	repositoriesUpdateChan := make(chan []JsonObject)
 	// assumes update time is shorter than refresh tick
 	go updateCache(log, repositoriesUpdateChan, eventPageUrl, refresh, maxCall, authorizationHeader)
 	for {
@@ -64,23 +68,23 @@ func manageUpdate(log logrus.FieldLogger, repositoriesChan chan<- []Repository, 
 	}
 }
 
-func retrieveRepositoriesData(log logrus.FieldLogger, eventPageUrl string, maxCall int, authorizationHeader string) []Repository {
+func retrieveRepositoriesData(log logrus.FieldLogger, eventPageUrl string, maxCall int, authorizationHeader string) []JsonObject {
 	urls := make(map[string]empty, 100)
 	for i := 1; len(urls) < 100; i++ {
 		extractRepositoriesUrl(log, urls, eventPageUrl, authorizationHeader, i)
 	}
 
-	retrievers := make([]func(chan<- Repository), len(urls))
+	retrievers := make([]func(chan<- JsonObject), len(urls))
 	for url := range urls {
 		urlCopy := url // avoid closure capture
-		retrievers = append(retrievers, func(repositoryChan chan<- Repository) {
+		retrievers = append(retrievers, func(repositoryChan chan<- JsonObject) {
 			retrieveRepositoryData(log, repositoryChan, urlCopy, authorizationHeader)
 		})
 	}
 	return limitedconcurrent.LaunchLimited(retrievers, maxCall)
 }
 
-func updateCache(log logrus.FieldLogger, repositoriesUpdateChan chan<- []Repository, eventPageUrl string, refresh time.Duration, maxCall int, authorizationHeader string) {
+func updateCache(log logrus.FieldLogger, repositoriesUpdateChan chan<- []JsonObject, eventPageUrl string, refresh time.Duration, maxCall int, authorizationHeader string) {
 	for range time.Tick(refresh) {
 		if data := retrieveRepositoriesData(log, eventPageUrl, maxCall, authorizationHeader); data != nil {
 			repositoriesUpdateChan <- data
@@ -95,38 +99,75 @@ func extractRepositoriesUrl(log logrus.FieldLogger, urls map[string]empty, event
 
 	data := githubApiGetRequest(log, urlBuilder.String(), authorizationHeader)
 
-	var events []map[string]any
+	var events []JsonObject
 	if err := json.Unmarshal(data, &events); err != nil {
 		log.WithError(err).Error("Fail to parse event api response")
 		return
 	}
 
 	for _, event := range events {
-		if repo, ok := event["repo"].(map[string]any); ok {
+		if repo, ok := event["repo"].(JsonObject); ok {
 			if repoUrl, _ := repo["url"].(string); repoUrl != "" {
-				urls[repoUrl] = empty{}
+				urls[repoUrl] = marker
 			}
 		}
 	}
 }
 
-func retrieveRepositoryData(log logrus.FieldLogger, repositoryChan chan<- Repository, repositoryUrl string, authorizationHeader string) {
+func retrieveRepositoryData(log logrus.FieldLogger, repositoryChan chan<- JsonObject, repositoryUrl string, authorizationHeader string) {
 	repositoryData := githubApiGetRequest(log, repositoryUrl, authorizationHeader)
 	if len(repositoryData) == 0 {
 		return
 	}
 
-	fullName, owner, name := "todo", "todo", "todo"
-
-	languagesUrl := "todo"
-	languagesData := githubApiGetRequest(log, languagesUrl, authorizationHeader)
-	if len(languagesData) == 0 {
+	var repository JsonObject
+	err := json.Unmarshal(repositoryData, &repository)
+	if err != nil {
+		log.WithError(err).Error("Fail to parse repository api response")
 		return
 	}
 
-	languages := map[string]int{}
-	// TODO
-	repositoryChan <- MakeRepository(fullName, owner, name, languages)
+	cleanedRepository := make(JsonObject, cleanedSize)
+	for key, value := range repository {
+		if _, ok := keepField[key]; ok {
+			cleanedRepository[key] = value
+			continue
+		}
+
+		if subKey, ok := flattenField[key]; ok {
+			if castedValue, okCast := value.(JsonObject); okCast {
+				cleanedRepository[key] = castedValue[subKey]
+				continue
+			}
+			log.WithField("flattenField", key).Error("Unable to flatten : can not cast to JsonObject")
+			return
+		}
+
+		newKey, ok := fetchField[key]
+		if !ok {
+			continue
+		}
+
+		url, _ := value.(string)
+		if url != "" {
+			log.WithField(key, value).Error("Unable to fetch url : empty or non string")
+			return
+		}
+
+		data := githubApiGetRequest(log, url, authorizationHeader)
+		if len(data) == 0 {
+			return
+		}
+
+		var parsed any
+		if err = json.Unmarshal(data, &parsed); err != nil {
+			log.WithError(err).Error("Fail to parse fetched response")
+			return
+		}
+		cleanedRepository[newKey] = parsed
+	}
+
+	repositoryChan <- cleanedRepository
 }
 
 func githubApiGetRequest(log logrus.FieldLogger, callUrl string, authorizationHeader string) []byte {
