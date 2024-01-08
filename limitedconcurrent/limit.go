6d8a34bc091ff3cbc69d699d2f36ec1390c423c5
Denis Vaumoron <dvaumoron@gmail.com>
2024-01-08 08:33:52 +0100

    small improvement

--- limitedconcurrent/limit.go
+++ limitedconcurrent/limit.go
@@ -1,7 +1,5 @@
 package limitedconcurrent
 
-import "sync"
-
 type empty = struct{}
 
 func LaunchLimited[T any](senders []func(chan<- T), limit int) []T {
@@ -16,19 +14,23 @@ func LaunchLimited[T any](senders []func(chan<- T), limit int) []T {
 }
 
 func manageLaunch[T any](outputChan chan<- T, senders []func(chan<- T), limit int) {
-	guard := make(chan empty, limit) // set a limited number of "concurrent slot"
-	var waitGroup sync.WaitGroup
-	waitGroup.Add(len(senders))
+	guard := make(chan empty, limit) // initialize a limited number of "concurrent slot"
+	done := func() {
+		<-guard // release a concurrent slot
+	}
+
 	for _, sender := range senders {
 		guard <- empty{}     // take a concurrent slot (block until one is available)
 		senderCopy := sender // avoid closure capture
 		go func() {
+			defer done() // use defer because sender can panic
 			senderCopy(outputChan)
-			<-guard // return the concurrent slot
-			waitGroup.Done()
 		}()
 	}
 
-	waitGroup.Wait()  // all work is done
-	close(outputChan) // no more sending
+	// fill all available slot
+	for i := 0; i < limit; i++ {
+		guard <- empty{}
+	}
+	close(outputChan) // all work is done, no more sending
 }
